f16dd7122b1391bf1fe7235e87c626e3a33aca4e
Janes Saenz <janes_saenz@hotmail.com>
2025-12-22 12:38:53 -0500

    store implements

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -17,6 +17,13 @@ export default function RootLayout() {
     <Provider store={store}>
       <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
             <Stack>
+              <Stack.Screen 
+              name="login" 
+              options={{ 
+                headerShown: false 
+              }}
+
+              />
               <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
               <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
             </Stack>
